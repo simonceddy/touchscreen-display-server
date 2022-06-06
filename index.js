9d9e8ea071717488234cb7ff0be7e3c0efc120be
Simon Eddy <simon@simoneddy.com.au>
2022-06-07 04:51:38 +1000

    small updates to app

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-/* eslint-disable no-unused-vars */
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const config = require('./config');
 const db = require('./db/db');
-const { Category, Item } = require('./db/models');
 const routes = require('./routes');
-
-function saveModels() {
-  const c = new Category({
-    title: 'Test',
-    key: 'test',
-    items: [
-      new Item({
-        title: 'Test item',
-      }),
-      new Item({
-        title: 'Another Test item',
-      }),
-    ]
-  });
-
-  async function saveModel() {
-    const r = await c.save();
-    return r;
-  }
-
-  saveModel()
-    .then((val) => console.log(val))
-    .catch(console.error);
-}
 
 async function connect() {
   await db.connect('mongodb://localhost:27017/display');
@@ -39,6 +14,9 @@ connect()
   .catch(console.error);
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost'
+}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
