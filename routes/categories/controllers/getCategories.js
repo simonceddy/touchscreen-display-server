481bbc3b474d9ec4cef9f6f68e6dcb4ac68a9f0a
Simon Eddy <simon@simoneddy.com.au>
2022-10-15 15:41:37 +1100

    bogan fix for subcategory item counts

--- routes/categories/controllers/getCategories.js
+++ routes/categories/controllers/getCategories.js
@@ -1,4 +1,5 @@
 const { Category } = require('../../../db/models');
+const getItemsFor = require('../../../util/db/getItemsFor');
 const getCategoryQuery = require('../../../util/getCategoryQuery');
 
 function getCategories(req, res) {
@@ -12,9 +13,22 @@ function getCategories(req, res) {
       }
       const data = await Promise.all(results.map(async (c) => {
         const items = await c.getItems();
+
+        let subs;
+        if (c.categories) {
+          subs = await Promise.all(c.categories.map(async (s) => {
+            const subItems = await getItemsFor(c.key, s.key);
+            return {
+              ...s.toObject(),
+              items: subItems
+            };
+          }));
+        }
+
         return {
           ...c.toObject(),
-          items
+          items,
+          categories: subs
         };
       }));
       return res.json(data);
