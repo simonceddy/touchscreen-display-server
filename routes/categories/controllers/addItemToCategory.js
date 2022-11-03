fc2159829ecf0ab400699f9d35cd535128125449
Simon Eddy <simon@simoneddy.com.au>
2022-11-04 04:44:56 +1100

    error check on add item

--- routes/categories/controllers/addItemToCategory.js
+++ routes/categories/controllers/addItemToCategory.js
@@ -1,9 +1,8 @@
+/* eslint-disable consistent-return */
 // const { Category } = require('../../../db/models');
 const saveItem = require('../../../util/db/saveItem');
 
 async function addItemToCategory(req, res) {
-  // Add item to the given category
-  // TODO use $push mongo operator
   const { key, subKey } = req.params;
   if (!req.body.title) {
     return res.status(500).json({
@@ -11,39 +10,25 @@ async function addItemToCategory(req, res) {
       message: 'Title is required'
     });
   }
-  const i = await saveItem({
-    title: req.body.title,
-    body: req.body.body || null,
-    media: req.body.media || [],
-    category: key,
-    subCategory: subKey || null,
-    thumbnail: req.body.thumbnail || null
-  });
+  try {
+    const i = await saveItem({
+      title: req.body.title,
+      body: req.body.body || null,
+      media: req.body.media || [],
+      category: key,
+      subCategory: subKey || null,
+      thumbnail: req.body.thumbnail || null
+    });
 
-  if (i) {
-    return res.json(i);
+    if (i) {
+      return res.json(i);
+    }
+  } catch (e) {
+    return res.json({
+      message: `an error occurred: ${e.message}`,
+      e
+    });
   }
-  return res.json('an error occurred');
-  // return Category.findOneAndUpdate(
-  //   { key },
-  //   {
-  //     $push: {
-  //       items: {
-  //         title: req.body.title,
-  //         body: req.body.body || '',
-  //         thumbnail: req.body.thumbnail,
-  //         media: req.body.media
-  //       }
-  //     }
-  //   },
-  //   { new: true, safe: true }
-  // )
-  //   .exec()
-  //   .then((result) => {
-  //     console.log(result);
-  //     return res.json(result);
-  //   })
-  //   .catch(console.error);
 }
 
 module.exports = addItemToCategory;
